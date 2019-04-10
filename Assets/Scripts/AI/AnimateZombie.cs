d97a27f83cdf571497d4d668a5ddaa394d1700e9
Alexander Lövberg <lovbergalexander@gmail.com>
2019-04-10 10:17:34 +0200

    new build

--- Assets/Scripts/AI/AnimateZombie.cs
+++ Assets/Scripts/AI/AnimateZombie.cs
@@ -2,22 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimateZombie : MonoBehaviour {
+public class AnimateZombie : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float smoothing = .5f;
 
-	Animator animator;
-	Rigidbody rigidbody;
+    Animator animator;
+    Rigidbody rigidbody;
 
-	void Start(){
-		animator = GetComponentInChildren<Animator>();
-		// velocity = GetComponent<Velocity>();
-		rigidbody = GetComponent<Rigidbody>();
-	}
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        // velocity = GetComponent<Velocity>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
 
-	void FixedUpdate () {
-		Vector3 velo = rigidbody.velocity;
-		velo = transform.InverseTransformDirection(velo);
+    void FixedUpdate()
+    {
+        Vector3 velo = rigidbody.velocity;
+        velo = transform.InverseTransformDirection(velo);
 
-		animator.SetFloat("velocityX", velo.x);
-		animator.SetFloat("velocityY", velo.z);
-	}
+        animator.SetFloat("velocityX", velo.x, smoothing, Time.deltaTime);
+        animator.SetFloat("velocityY", velo.z, smoothing, Time.deltaTime);
+    }
 }
